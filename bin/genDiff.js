e6b3e9cd968d3c030004691c979c24efc076c82a
Viktor Smorodina <smorodinaspb@yandex.ru>
2021-02-15 16:13:47 +0300

    add options and arguments

--- bin/genDiff.js
+++ bin/genDiff.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
 import commander from 'commander';
 
-commander.version('0.0.1');
-commander.description('Compares two configuration files and shows a difference.');
-commander.parse(process.argv);
+commander
+  .version('0.0.1')
+  .arguments('<filepath1> <filepath2>')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'output format')
+  .parse(process.argv);
