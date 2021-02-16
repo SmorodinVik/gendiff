a33068e514173a45e0681a570fc91cc1ee567ef3
Viktor Smorodina <smorodinaspb@yandex.ru>
2021-02-16 15:44:02 +0300

    add and setup jest

--- Makefile
+++ Makefile
@@ -4,3 +4,5 @@ try:
 	gendiff /Users/Smorodina/Desktop/Projects/frontend-project-lvl2/testjsons/file1.json testjsons/file2.json
 lint:
 	npx eslint .
+test:
+	npm run test
---
+++ __tests__/findDiff.test.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-undef */
+import path from 'path';
+import findDiff from '../src/findDiff.js';
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+
+test('find differences', () => {
+  const path1 = getFixturePath('file1.json');
+  const path2 = getFixturePath('file2.json');
+  const result = findDiff(path1, path2);
+  const expected = '{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}';
+  expect(result).toEqual(expected);
+});
