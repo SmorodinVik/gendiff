982bc82abca99e9024b12d2e17cf9fbbfc51aa7b
Viktor Smorodina <smorodinaspb@yandex.ru>
2021-02-18 23:53:49 +0300

    add test for stylish

--- __tests__/findDiff.test.js
+++ __tests__/findDiff.test.js
@@ -1,19 +1,66 @@
 import path from 'path';
 import findDiff from '../src/findDiff.js';
+import readAndParse from '../src/parsers.js';
+import stylish from '../src/stylish.js';
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-const expected = '{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}';
+const expected = `{
+    common: {
+      + follow: false
+        setting1: Value 1
+      - setting2: 200
+      - setting3: true
+      + setting3: null
+      + setting4: blah blah
+      + setting5: {
+            key5: value5
+        }
+        setting6: {
+            doge: {
+              - wow: 
+              + wow: so much
+            }
+            key: value
+          + ops: vops
+        }
+    }
+    group1: {
+      - baz: bas
+      + baz: bars
+        foo: bar
+      - nest: {
+            key: value
+        }
+      + nest: str
+    }
+  - group2: {
+        abc: 12345
+        deep: {
+            id: 45
+        }
+    }
+  + group3: {
+        deep: {
+            id: {
+                number: 45
+            }
+        }
+        fee: 100500
+    }
+}`;
 
 describe.each([
   ['file1.json', 'file2.json', 'json/json'],
-  ['file3.yaml', 'file4.yml', 'yaml/yml'],
-  ['file1.json', 'file4.yml', 'json/yml'],
+  ['file3.yml', 'file4.yaml', 'yaml/yml'],
+  ['file1.json', 'file4.yaml', 'json/yml'],
 ])('Find differences:', (file1, file2, description) => {
   test(`${description}`, () => {
     const path1 = getFixturePath(file1);
     const path2 = getFixturePath(file2);
-    const result = findDiff(path1, path2);
-    expect(result).toEqual(expected);
+    const data1 = readAndParse(path1);
+    const data2 = readAndParse(path2);
+    const result = findDiff(data1, data2);
+    expect(stylish(result)).toEqual(expected);
   });
 });
