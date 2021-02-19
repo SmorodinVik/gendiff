d57692577214721470b4f6bee40c6ac3978e0739
Viktor Smorodina <smorodinaspb@yandex.ru>
2021-02-19 16:42:51 +0300

    update buildDiffTree

--- src/buildDiffTree.js
+++ src/buildDiffTree.js
@@ -1,26 +1,25 @@
 import _ from 'lodash';
 
+const isObjNotArr = (val) => _.isObject(val) && !_.isArray(val);
+
 const buildDiffTree = (obj1, obj2 = obj1) => {
   const keys = Object.keys({ ...obj1, ...obj2 }).sort();
   const result = keys.reduce((acc, key) => {
     const value1 = obj1[key];
     const value2 = obj2[key];
     if (value1 === undefined) {
-      const parsedValue2 = _.isObject(value2) && !_.isArray(value2) ? buildDiffTree(value2) : value2;
+      const parsedValue2 = isObjNotArr(value2) ? buildDiffTree(value2) : value2;
       acc.push({ key, value: parsedValue2, status: '+' });
     } else if (value2 === undefined) {
-      const parsedValue1 = _.isObject(value1) && !_.isArray(value1) ? buildDiffTree(value1) : value1;
+      const parsedValue1 = isObjNotArr(value1) ? buildDiffTree(value1) : value1;
       acc.push({ key, value: parsedValue1, status: '-' });
     } else if (value1 === value2 && !_.isObject(value1)) {
       acc.push({ key, value: value1, status: ' ' });
-    } else if (_.isObject(value1)
-        && !_.isArray(value1)
-        && _.isObject(value2)
-        && !_.isArray(value2)) {
+    } else if (isObjNotArr(value1) && isObjNotArr(value2)) {
       acc.push({ key, value: buildDiffTree(value1, value2), status: ' ' });
     } else {
-      const parsedValue1 = _.isObject(value1) && !_.isArray(value1) ? buildDiffTree(value1) : value1;
-      const parsedValue2 = _.isObject(value2) && !_.isArray(value2) ? buildDiffTree(value2) : value2;
+      const parsedValue1 = isObjNotArr(value1) ? buildDiffTree(value1) : value1;
+      const parsedValue2 = isObjNotArr(value2) ? buildDiffTree(value2) : value2;
       acc.push({ key, value: parsedValue1, status: '-' });
       acc.push({ key, value: parsedValue2, status: '+' });
     }
