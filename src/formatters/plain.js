84425b72ed902e668fdefb56ed667e6c8889e702
Viktor Smorodina <smorodinaspb@yandex.ru>
2021-02-19 19:46:30 +0300

    update plain

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -7,38 +7,30 @@ const formatValue = (val) => {
   return _.isString(val) ? `'${val}'` : val;
 };
 
-const plain = (dataTree) => {
-  const fn = (data, parents = '') => {
-    const result = data.reduce((acc, current, i) => {
-      const { key, value, status } = current;
-      const keyWithParents = `${parents}${key}`;
-      if (status === '-') {
-        if (data[i + 1] && data[i + 1].key === key) {
-          const oldValue = formatValue(value);
-          const newValue = formatValue(data[i + 1].value);
-          acc.push(`Property '${keyWithParents}' was updated. From ${oldValue} to ${newValue}`);
-          return acc;
-        }
-        acc.push(`Property '${keyWithParents}' was removed`);
-        return acc;
-      }
-      if (status === '+') {
-        if (data[i - 1] && data[i - 1].key === key) {
-          return acc;
-        }
-        acc.push(`Property '${keyWithParents}' was added with value: ${formatValue(value)}`);
-        return acc;
-      }
-      if (_.isArray(value)) {
-        acc.push(fn(value, `${keyWithParents}.`));
-        return acc;
-      }
-      return acc;
-    }, []).flat();
-    return result.join('\n');
-  };
+const fn = (data, parents = '') => {
+  const result = data.reduce((acc, current, i) => {
+    const { key, value, status } = current;
+    const keyWithParents = `${parents}${key}`;
 
-  return fn(dataTree);
+    if (status === '-') {
+      if (data[i + 1] && data[i + 1].key === key) {
+        const oldValue = formatValue(value);
+        const newValue = formatValue(data[i + 1].value);
+        acc.push(`Property '${keyWithParents}' was updated. From ${oldValue} to ${newValue}`);
+      } else {
+        acc.push(`Property '${keyWithParents}' was removed`);
+      }
+    } else if (status === '+') {
+      if (data[i - 1] && data[i - 1].key === key) {
+        return acc;
+      }
+      acc.push(`Property '${keyWithParents}' was added with value: ${formatValue(value)}`);
+    } else if (_.isArray(value)) {
+      acc.push(fn(value, `${keyWithParents}.`));
+    }
+    return acc;
+  }, []).flat();
+  return result.join('\n');
 };
 
-export default plain;
+export default (dataTree) => fn(dataTree);
