20b6a4a72fe436adbd973d09de487f12a6db27cc
Viktor Smorodina <smorodinaspb@yandex.ru>
2021-02-19 17:00:13 +0300

    update buildDiffTree

--- src/buildDiffTree.js
+++ src/buildDiffTree.js
@@ -7,19 +7,17 @@ const buildDiffTree = (obj1, obj2 = obj1) => {
   const result = keys.reduce((acc, key) => {
     const value1 = obj1[key];
     const value2 = obj2[key];
+    const parsedValue1 = isObjNotArr(value1) ? buildDiffTree(value1) : value1;
+    const parsedValue2 = isObjNotArr(value2) ? buildDiffTree(value2) : value2;
     if (value1 === undefined) {
-      const parsedValue2 = isObjNotArr(value2) ? buildDiffTree(value2) : value2;
       acc.push({ key, value: parsedValue2, status: '+' });
     } else if (value2 === undefined) {
-      const parsedValue1 = isObjNotArr(value1) ? buildDiffTree(value1) : value1;
       acc.push({ key, value: parsedValue1, status: '-' });
     } else if (value1 === value2 && !_.isObject(value1)) {
       acc.push({ key, value: value1, status: ' ' });
     } else if (isObjNotArr(value1) && isObjNotArr(value2)) {
       acc.push({ key, value: buildDiffTree(value1, value2), status: ' ' });
     } else {
-      const parsedValue1 = isObjNotArr(value1) ? buildDiffTree(value1) : value1;
-      const parsedValue2 = isObjNotArr(value2) ? buildDiffTree(value2) : value2;
       acc.push({ key, value: parsedValue1, status: '-' });
       acc.push({ key, value: parsedValue2, status: '+' });
     }
